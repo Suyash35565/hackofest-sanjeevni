5e20910f69ffbef08509ae3a0d0cce9b124a3b56
Suyash35565 <162812074+Suyash35565@users.noreply.github.com>
2024-04-07 21:41:13 +0530

    Update login_doc.php

--- ls/include/login_doc.php
+++ ls/include/login_doc.php
@@ -13,7 +13,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
         session_start();
         $_SESSION['loggedin']=true;
         $_SESSION['username']=$username;
-        header("location: \Mynew\d_dashboard.php");
+        header("location: \Sanjeevanifinal\d_dashboard.php");
     }    
     else{
         $showError="Invalid Credentials";
